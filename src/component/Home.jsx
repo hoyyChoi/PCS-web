7ba7dc8f3bfc3c14bdb6da846c195c30a8932dfb
hoyychoi <hoyeon8165@gmail.com>
2022-09-02 16:02:09 +0900

    로그상태에 따른 home,navbar 일부변경

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import ArticleList from './ArticleList'
 import Populartags from './Populartags'
 
-const Home = () => {
+const Home = ({auth}) => {
+
   return (
     <div class="home-page">
 
-    <div class="banner">
+    <div class="banner" style={auth?{display:'none'}:{display:'flex'}}>
         <div class="container">
             <h1 class="logo-font">conduit</h1>
             <p>A place to share your knowledge.</p>
@@ -20,7 +21,7 @@ const Home = () => {
                 <div class="feed-toggle">
                     <ul class="nav nav-pills outline-active">
                         <li class="nav-item">
-                            <a class="nav-link disabled" href="">Your Feed</a>
+                            <a class="nav-link disabled" href="" style={auth?{display:'flex'}:{display:'none'}}>Your Feed</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link active" href="">Global Feed</a>
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,34 +1,42 @@
 import React from 'react'
 
 // <li> 테그에 있는 데이터들을 배열로 이동시킨후, map 내장함수를 이용해서 뿌려준다
-// 로그인 했을떄랑 안했을때 보여지는게 다른데 그 부분을 배열로 표현 할 수 있는지..
-const Navbar = () => {
+// 로그인 했을떄랑 안했을때 보여지는게 다른데 그 부분을 배열로 표현 할 수 있는지.
+
+// Login/Logout 시 Navbar의 내용 구분
+// Login : Home / NewArticle / Settings / username
+// Logout : Home / Sign in / Sign up
+
+// Api 연동 X
+// ->임의로 boolean 값을 state 변수에 넣어 사용 (UI로 확인하기 위해)
+// True : login
+// False : logout
+// { user(state변수)? <>로그인했을때 네브바</> : <>로그인 안했을때 네브바</> }
+
+
+
+
+const Navbar = ({auth}) => {
+    const notLoggedIn = ['Home','Sign in','Sign up']
+    const isLoggedIn = ['Home','New Article','Settings','username']
+
+    
   return (
     
     <nav class="navbar navbar-light">
         <div class="container">
             <a class="navbar-brand" href="/">conduit</a>
+            
+            
             <ul class="nav navbar-nav pull-xs-right">
-                <li class="nav-item">
-                    <a class="nav-link active" href="">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="">
-                        <i class="ion-compose"></i>&nbsp;New Article
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="">
-                        <i class="ion-gear-a"></i>&nbsp;Settings
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="">Sign in</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="">Sign up</a>
-                </li>
+                {auth?isLoggedIn.map((data)=>{
+                    return <li class="nav-item"><a class="nav-link active" href="">{data}</a></li>
+                }):notLoggedIn.map((data)=>{
+                    return <li class="nav-item"><a class="nav-link active" href="">{data}</a></li>
+                })}
             </ul>
+
+              
         </div>
     </nav>
     
@@ -36,3 +44,6 @@ const Navbar = () => {
 }
 
 export default Navbar
+
+
+
