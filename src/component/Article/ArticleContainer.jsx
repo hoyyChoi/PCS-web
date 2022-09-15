e15e0abbddcea23b190b3260efa65eede3bf716c
hoyychoi <hoyeon8165@gmail.com>
2022-09-15 18:07:05 +0900

    [#60] 로그 유무상태에 따른 comment 적는 부분 변경

--- src/component/Article/ArticleContainer.jsx
+++ src/component/Article/ArticleContainer.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
+import { useRecoilValue } from 'recoil'
+import { authState } from '../../atoms/auth'
 import ArticleContents from './ArticleContents'
 import ShowComment from './ShowComment'
 import WriteCommet from './WriteCommet'
+import { Link } from 'react-router-dom'
 
 const ArticleContainer = ({data}) => {
+
+  const auth = useRecoilValue(authState)
   return (
     <div className="container page">
 
     <ArticleContents data={data}/>
-    <WriteCommet />
+    {auth?<WriteCommet />:<div style={{textAlign:'center', margin:'10px'}}>
+      <Link to="/login">sign in</Link> or <Link to="/register">sign up</Link>
+    </div>}
     <ShowComment />
 
 </div>
