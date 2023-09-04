dc38b905ceb7bb16d635623d4572884dfd6fce4b
hoyyChoi <hoyeon8165@gmail.com>
2023-09-05 03:23:16 +0900

    feat : change article container

--- src/component/Article/ArticleContainer.jsx
+++ src/component/Article/ArticleContainer.jsx
@@ -1,29 +1,12 @@
-import React from 'react'
-import { useRecoilValue } from 'recoil'
-import { authState,commentsState } from '../../atoms/auth'
-import ArticleContents from './ArticleContents'
-import ShowComment from './ShowComment'
-import WriteCommet from './WriteCommet'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import ArticleContents from './ArticleContents';
 
 const ArticleContainer = ({data}) => {
+	return (
+		<div className="container page">
+			<ArticleContents data={data} />
+		</div>
+	);
+};
 
-  const auth = useRecoilValue(authState)
-  const comments = useRecoilValue(commentsState)
-
-  return (
-    <div className="container page">
-
-    <ArticleContents data={data}/>
-    <div style={{textAlign:'center'}}>댓글 갯수 : {comments.length}개</div>
-    {auth ? <WriteCommet />
-    :<div style={{textAlign:'center', margin:'10px'}}>
-      <Link to="/login">sign in</Link> or <Link to="/register">sign up</Link>
-    </div>}
-    <ShowComment />
-
-</div>
-  )
-}
-
-export default ArticleContainer
+export default ArticleContainer;
