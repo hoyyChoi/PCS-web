38a275d55f55fc1011b9e6dfe6d949361608f9de
hoyyChoi <hoyeon8165@gmail.com>
2023-09-05 03:21:49 +0900

    fix : recoil

--- src/atoms/auth.jsx
+++ src/atoms/auth.jsx
@@ -1,51 +1,9 @@
-import {atom} from 'recoil'
+import {atom} from 'recoil';
+import {recoilPersist} from 'recoil-persist';
+const {persistAtom} = recoilPersist();
 
-
-export const authState = atom({
-    key: 'src/atoms/auth.jsx-authState', 
-    default: false
-  });
-
-
-export const userState = atom({
-  key: 'src/atoms/auth.jsx-userState', 
-  default: {
-    username:'',
-    email:'',
-    token:'',
-    bio:null,
-    image:'https://api.realworld.io/images/smiley-cyrus.jpeg',
-  }
-})
-
-export const profileState = atom({
-  key: 'src/atoms/auth.jsx-profileState',
-  default :{
-    username:'',
-    bio:null,
-    image:'https://api.realworld.io/images/smiley-cyrus.jpeg',
-    following:false
-  }
-})
-
-export const slugState = atom({
-  key:'src/atoms/auth.jsx-slugState',
-  default:''
-})
-
-export const commentsState = atom({
-  key:'src/atoms/auth.jsx-commentsState',
-  default:[]
-})
-
-
-
-// export const newArticleState = atom({
-//   key: 'src/atoms/auth.jsx-newArticleState',
-//   default:{
-//     title: '',
-//     description: '',
-//     body: '',
-//     tagList:[]
-//   }
-// })
+export const eachArticeState = atom({
+	key: 'src/atoms/auth.jsx-eachArticeState',
+	default: {},
+	effects_UNSTABLE: [persistAtom],
+});
