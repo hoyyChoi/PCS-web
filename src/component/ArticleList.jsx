6295ed58e0c3543c68853dbed086ca91f338a4dc
hoyyChoi <hoyeon8165@gmail.com>
2023-09-05 03:22:10 +0900

    feat : change article list

--- src/component/ArticleList.jsx
+++ src/component/ArticleList.jsx
@@ -1,35 +1,32 @@
-import React from 'react'
-import { useRecoilState } from 'recoil'
-import { useNavigate } from 'react-router-dom'
-import { slugState } from '../atoms/auth'
-import WritterInfo from './WritterInfo'
+import React from 'react';
+import {useNavigate} from 'react-router-dom';
+import WritterInfo from './WritterInfo';
 
 const ArticleList = ({data}) => {
+	const navigate = useNavigate();
+	const spaceArticle = () => {
+		navigate(`/article/${data.articleId}`);
+	};
 
-  const navigate = useNavigate()
-  const [slug,setSlug] = useRecoilState(slugState)
-  const spaceArticle = () =>{
-    localStorage.setItem('slug',data.slug)
-    setSlug(data.slug)
-    navigate(`/article/${localStorage.getItem('slug')}`)
-  }
+	return (
+		<div className="article-preview">
+			<div className="article-meta">
+				<WritterInfo data={data} />
+				<div className="btn btn-outline-primary btn-sm pull-xs-right">
+					<i className="ion-heart"></i>
+				</div>
+			</div>
+			<div
+				onClick={spaceArticle}
+				className="preview-link"
+				style={{cursor: 'pointer'}}
+			>
+				<h1>{data.title}</h1>
+				<p>{data.description}</p>
+				<span>더 보기...</span>
+			</div>
+		</div>
+	);
+};
 
-  // data.slug를 상태에 업데이트를 시켜서 Api 연결 get slug -> ARTICLE DETAIL 컴포넌트 -> 업데이트된 정보를  뿌려준다.
-  return (
-<div className="article-preview">
-    <div className="article-meta">
-        <WritterInfo data={data}/>    
-            <button className="btn btn-outline-primary btn-sm pull-xs-right">
-                    <i className="ion-heart"></i> 
-            </button> 
-    </div>
-    <div onClick={spaceArticle} className="preview-link" style={{cursor:'pointer'}}>
-        <h1>{data.title}</h1>
-        <p>{data.description}</p>
-        <span>Read more...</span>
-    </div>
-</div>
-  )
-}
-
-export default ArticleList
+export default ArticleList;
