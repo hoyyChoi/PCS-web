0abb9d713540d26c19cb0b3bee449573e5cada80
hoyyChoi <hoyeon8165@gmail.com>
2023-09-05 03:22:34 +0900

    feat : change newArticle

--- src/component/Newarticle.jsx
+++ src/component/Newarticle.jsx
@@ -1,72 +1,101 @@
-import React from 'react'
-import { useState } from 'react'
-import { createArticle } from '../remote/index'
-import { useRecoilValue } from 'recoil'
-import { userState } from '../atoms/auth'
-import { useRecoilState } from 'recoil'
-import { slugState } from '../atoms/auth'
-import { useNavigate } from 'react-router-dom'
+import React from 'react';
+import {useState} from 'react';
+import {postArticle} from '../remote/index';
+import {useNavigate} from 'react-router-dom';
 
 const Newarticle = () => {
+	const [title, setTitle] = useState('');
+	const [description, setDescription] = useState('');
+	const [body, setBody] = useState('');
+	const [tag1, setTag1] = useState('STUDY');
+	const [tag2, setTag2] = useState('HEALTH');
+	const [tag3, setTag3] = useState('DRINK');
 
-    const user = useRecoilValue(userState)
-    const [slug,setSlug] = useRecoilState(slugState)
+	const navigate = useNavigate();
 
-    const [title,setTitle] = useState('')
-    const [description,setDescription] = useState('')
-    const [body,setBody] = useState('')
-    const [tag,setTag] = useState([''])
+	const submitArticle = e => {
+		e.preventDefault();
+		const tagList = [tag1, tag2, tag3];
+		postArticle({title, description, body, tagList})
+			.then(res => {
+				navigate(`/article/${res.data.articleId}`);
+			})
+			.catch(err => {
+				alert('태그는 STUDY,HEALTH,DRINK,OTHER 가능합니다');
+			});
+	};
 
-    const navigate =useNavigate()
+	return (
+		<div className="editor-page">
+			<div className="container page">
+				<div className="row">
+					<div className="col-md-10 offset-md-1 col-xs-12">
+						<form onSubmit={e => submitArticle(e)}>
+							<fieldset>
+								<fieldset className="form-group">
+									<input
+										type="text"
+										className="form-control form-control-lg"
+										placeholder="제목"
+										value={title}
+										onChange={e => setTitle(e.currentTarget.value)}
+									/>
+								</fieldset>
+								<fieldset className="form-group">
+									<input
+										className="form-control"
+										rows="8"
+										placeholder="소제목"
+										value={description}
+										onChange={e => setDescription(e.currentTarget.value)}
+									></input>
+								</fieldset>
+								<fieldset className="form-group">
+									<textarea
+										className="form-control"
+										rows="8"
+										placeholder="적고 싶은 내용"
+										value={body}
+										onChange={e => setBody(e.currentTarget.value)}
+									></textarea>
+								</fieldset>
+								<fieldset className="form-group tag-form">
+									<input
+										type="text"
+										className="form-control"
+										placeholder="Enter tags"
+										value={tag1}
+										onChange={e => setTag1(e.currentTarget.value)}
+									/>
+									<input
+										type="text"
+										className="form-control"
+										placeholder="Enter tags"
+										value={tag2}
+										onChange={e => setTag2(e.currentTarget.value)}
+									/>
+									<input
+										type="text"
+										className="form-control"
+										placeholder="Enter tags"
+										value={tag3}
+										onChange={e => setTag3(e.currentTarget.value)}
+									/>
+									<div className="tag-list"></div>
+								</fieldset>
+								<button
+									className="btn btn-lg pull-xs-right btn-primary"
+									type="submit"
+								>
+									글 저장
+								</button>
+							</fieldset>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-    const submitArticle = (e)=>{
-        e.preventDefault()
-
-        createArticle({title,description,body,tag})
-        .then(res=>{
-            setSlug(res.data.article.slug)
-            localStorage.setItem('slug',res.data.article.slug)
-            navigate(`/article/${localStorage.getItem('slug')}`)
-        }).catch(err=>{
-            alert('이미 있는 제목입니다.')
-        })
-    }
-
-
-
-  return (
-    <div className="editor-page">
-    <div className="container page">
-        <div className="row">
-
-            <div className="col-md-10 offset-md-1 col-xs-12">
-                <form onSubmit={(e)=>submitArticle(e)}>
-                    <fieldset>
-                        <fieldset className="form-group">
-                            <input type="text" className="form-control form-control-lg" placeholder="Article Title" value={title} onChange={(e)=>setTitle(e.currentTarget.value)}/>
-                        </fieldset>
-                        <fieldset className="form-group">
-                            <input type="text" className="form-control" placeholder="What's this article about?" value={description} onChange={(e)=>setDescription(e.currentTarget.value)}/>
-                        </fieldset>
-                        <fieldset className="form-group">
-                            <textarea className="form-control" rows="8"
-                                      placeholder="Write your article (in markdown)" value={body} onChange={(e)=>setBody(e.currentTarget.value)}></textarea>
-                        </fieldset>
-                        <fieldset className="form-group">
-                            <input type="text" className="form-control" placeholder="Enter tags" value={tag} onChange={(e)=>setTag(e.currentTarget.value)}/>
-                            <div className="tag-list"></div>
-                        </fieldset>
-                        <button className="btn btn-lg pull-xs-right btn-primary" type="submit">
-                            Publish Article
-                        </button>
-                    </fieldset>
-                </form>
-            </div>
-
-        </div>
-    </div>
-</div>
-  )
-}
-
-export default Newarticle
+export default Newarticle;
