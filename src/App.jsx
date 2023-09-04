846e6b5365dd170cd6bb55396fac287f78899231
hoyyChoi <hoyeon8165@gmail.com>
2023-09-05 03:21:32 +0900

    fix : change structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,21 @@
-import './App.css'
-import Navbar from './component/Navbar'
-import Home from './component/Home'
-import Footer from './component/Footer'
-import { Routes, Route } from "react-router-dom";
-import Signin from './component/Signin';
-import Signup from './component/Signup';
-import Settings from './component/Settings';
+import './App.css';
+import Navbar from './component/Navbar';
+import Home from './component/Home';
+import {Routes, Route} from 'react-router-dom';
 import Newarticle from './component/Newarticle';
-import Profile from './component/Profile';
 import ArticleDetail from './component/Article/ArticleDetail';
-import {useRecoilState} from 'recoil'
-import {authState,userState} from './atoms/auth'
-import { useEffect } from 'react';
-import { getLoginUser } from './remote/index';
 
 function App() {
-
-  const [auth,setAuth] = useRecoilState(authState)
-  const [user,setUserState] = useRecoilState(userState)
-
-  useEffect(()=>{
-    if(localStorage.getItem('token')){
-      getLoginUser()
-      .then(res=>{
-        setUserState(res.data.user)
-        setAuth(true)
-      }).catch(err=>{
-        console.log(err)
-      })
-      
-    }else{
-      setAuth(false)
-    }
-  },[])
-  
-  return (
-    <div>
-      <Navbar/>
-        <Routes> 
-          <Route path='/' element={<Home />}/>
-          <Route path='/login' element={<Signin />}/>
-          <Route path='/register' element={<Signup />}/>
-          <Route path='/settings' element={<Settings />}/>
-          <Route path='/editor' element={<Newarticle />}/> 
-          <Route path='/:username' element={<Profile />}/> 
-          <Route path='/article/:slug' element={<ArticleDetail />}/> 
-        </Routes>
-      <Footer />
-    </div>
-  )
+	return (
+		<div>
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/editor" element={<Newarticle />} />
+				<Route path="/article/:id" element={<ArticleDetail />} />
+			</Routes>
+		</div>
+	);
 }
 
-export default App
+export default App;
