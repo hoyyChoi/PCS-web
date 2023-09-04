0dc99921a71e16ec282b97510be7f7a96956b1fa
hoyyChoi <hoyeon8165@gmail.com>
2023-09-05 03:23:41 +0900

    feat : change article title

--- src/component/Article/ArticleTitle.jsx
+++ src/component/Article/ArticleTitle.jsx
@@ -1,46 +1,53 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
-import { useRecoilValue } from "recoil";
-import { commentsState } from "../../atoms/auth";
-import { deleteSlugArticle } from "../../remote/index";
-import WritterInfo from "../WritterInfo";
-
+import React from 'react';
+import {useNavigate} from 'react-router-dom';
+import {deleteIdArticle} from '../../remote/index';
+import WritterInfo from '../WritterInfo';
 
 const ArticleTitle = ({data}) => {
+	const navigate = useNavigate();
 
-  const navigate = useNavigate()
-  
+	const deleteArticle = () => {
+		deleteIdArticle(data.articleId)
+			.then(res => {
+				navigate('/');
+			})
+			.catch(err => {
+				console.log(err);
+				alert('article은 지울 수 없습니다.');
+			});
+	};
 
-  const deleteArticle =()=>{
-    deleteSlugArticle(data.slug)
-    .then(res=>{
-      navigate('/')
-    }).catch(err=>{
-      console.log(err)
-      alert('다른 상대방의 article은 지울 수 없습니다.')
-    })
-  }
+	const putArticle = () => {
+		// 수정하기로 이동
+		alert('준비중입니다. 기달려주세요');
+	};
 
-  return (
-    <div className="banner">
-      <div className="container">
-        <h1>{data&&data.title}</h1>
+	return (
+		<div className="banner">
+			<div className="container">
+				<h1>{data.title}</h1>
 
-        <div className="article-meta">
-          <WritterInfo data={data}/>
-          <button className="btn btn-sm btn-outline-primary">
-            <i className="ion-heart"></i>
-            &nbsp; Favorite Post <span className="counter">({data&&data.favoritesCount})</span>
-          </button>
-          &nbsp;&nbsp;
-          <button className="btn btn-sm btn-outline-primary" onClick={deleteArticle}>
-            <i class="ion-trash-a"></i>
-            &nbsp; Delete 
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+				<div className="article-meta">
+					<WritterInfo data={data} />
+					<button
+						className="btn btn-sm btn-outline-primary"
+						onClick={putArticle}
+					>
+						<i className="ion-compose"></i>
+						&nbsp; 글 수정
+					</button>
+					&nbsp;&nbsp;
+					<button
+						className="btn btn-sm btn-outline-primary"
+						onClick={deleteArticle}
+					>
+						<i class="ion-trash-a"></i>
+						&nbsp; 글 지우기
+					</button>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default ArticleTitle;
