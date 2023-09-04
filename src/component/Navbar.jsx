00abb5d2932680abadaf38d1175f8cb2850a75c6
hoyyChoi <hoyeon8165@gmail.com>
2023-09-05 03:22:25 +0900

    feat : change navbar

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,10 +1,6 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { useRecoilValue, useRecoilState } from "recoil";
-import { authState, userState } from '../atoms/auth'
-import { getProfile } from "../remote/index";
-import { profileState } from "../atoms/auth";
-import { useNavigate } from "react-router-dom";
+import React from 'react';
+import {Link} from 'react-router-dom';
+
 // <li> 테그에 있는 데이터들을 배열로 이동 시킨후, map 내장함수를 이용해서 뿌려준다
 // 로그인 했을떄랑 안했을때 보여지는게 다른데 그 부분을 배열로 표현 할 수 있는지.
 
@@ -19,98 +15,24 @@ import { useNavigate } from "react-router-dom";
 // { user(state변수)? <>로그인했을때 네브바</> : <>로그인 안했을때 네브바</> }
 
 const Navbar = () => {
-  const notLoggedIn = ["Home", "Sign in", "Sign up"];
-  const isLoggedIn = ["Home", "New Article", "Settings", "username"];
+	return (
+		<nav className="navbar navbar-light">
+			<div className="container">
+				<Link className="navbar-brand" to="/">
+					<img src="/kusitm.svg" alt="큐시즘" width={50} />
+					<div>kusitms</div>
+				</Link>
 
-  const auth = useRecoilValue(authState)
-  const user = useRecoilValue(userState)  
-  const [profile,setProfile] = useRecoilState(profileState)
-  const navigate = useNavigate()
-
-  const spaceProfile = () =>{
-    getProfile(user.username)
-    .then((res)=>{
-        setProfile(res.data.profile)
-        navigate(`/${user.username}`)
-    }).catch((err)=>{
-        console.log(err)
-     })
-  }
-    
-
-  
-  return (
-    <nav className="navbar navbar-light">
-      <div className="container">
-        <Link className="navbar-brand" to='/'>
-          conduit
-        </Link>
-
-        <ul className="nav navbar-nav pull-xs-right">
-          {auth ? (
-            <div>
-              <li className="nav-item">
-                <Link className="nav-link active" to='/'>Home</Link>
-              </li>
-              <li className="nav-item">
-              <Link className="nav-link" to='/editor'>
-                  <i className="ion-compose"></i>&nbsp;New Article
-              </Link>
-              </li>
-              <li className="nav-item">
-              <Link className="nav-link" to='/settings'>
-                  <i className="ion-compose"></i>&nbsp;Settings
-              </Link>
-              </li>
-              <li className="nav-item">
-                  <div className="nav-link active" onClick={spaceProfile} style={{cursor:'pointer'}}>
-                      {user.username}
-                  </div>
-              </li>
-            </div>
-          ) : (
-            <div>
-              <li className="nav-item">
-                  <Link className="nav-link active" to='/'>
-                      Home
-                  </Link>
-              </li>
-              <li className="nav-item">
-                  <Link className="nav-link" to='/login'>
-                      Sign in
-                  </Link>
-              </li>
-              <li className="nav-item">
-                  <Link className="nav-link" to='/register'>
-                      Sign up
-                  </Link>
-              </li>
-            </div>
-          )}
-        </ul>
-      </div>
-    </nav>
-  );
+				<ul className="nav navbar-nav pull-xs-right">
+					<li className="nav-item">
+						<Link className="nav-link" to="/editor">
+							<i className="ion-compose"></i>&nbsp;글쓰기
+						</Link>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	);
 };
 
 export default Navbar;
-
-// {auth
-//     ? isLoggedIn.map((data) => {
-//         return (
-//           <li className="nav-item">
-//             <a className="nav-link active" href="">
-//               {data}
-//             </a>
-//           </li>
-//         );
-//       })
-//     : notLoggedIn.map((data) => {
-//         return (
-//           <li className="nav-item">
-//             <a className="nav-link active" href="/login">
-//               {data}
-//             </a>
-//           </li>
-//         );
-//       })}
