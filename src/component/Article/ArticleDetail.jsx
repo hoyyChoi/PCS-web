f876a1dee6cffa4372fc167b29a27c24c5b9d2fe
hoyyChoi <hoyeon8165@gmail.com>
2023-09-05 03:23:34 +0900

    feat : change article detail

--- src/component/Article/ArticleDetail.jsx
+++ src/component/Article/ArticleDetail.jsx
@@ -1,49 +1,29 @@
-import React from 'react'
-import ArticleContainer from './ArticleContainer'
-import ArticleTitle from './ArticleTitle'
-import { getSlugArticle,getLoginComment,getComment } from '../../remote/index'
-import { useEffect } from 'react'
-import { useRecoilValue, useRecoilState } from 'recoil'
-import { slugState, authState, commentsState } from '../../atoms/auth'
-import { useState } from 'react'
+import React from 'react';
+import ArticleContainer from './ArticleContainer';
+import ArticleTitle from './ArticleTitle';
+import {getIdArticle} from '../../remote/index';
+import {useEffect} from 'react';
+import {useRecoilState} from 'recoil';
+import {eachArticeState} from '../../atoms/auth';
+import {useParams} from 'react-router-dom';
 
 const ArticleDetail = () => {
+	const [Articledata, setArticleData] = useRecoilState(eachArticeState);
 
-  const slug = useRecoilValue(slugState)
-  const [data,setArticleData]=useState()
-  const [comments, setComments] = useRecoilState(commentsState);
-  const auth = useRecoilValue(authState)
+	let {id} = useParams();
 
-  useEffect(()=>{
-    getSlugArticle(localStorage.getItem('slug'))
-    .then(res=>{
-      setArticleData(res.data.article)
-    }).catch(err=>{
-      console.log(err)
-    })
-    auth ? (getLoginComment(localStorage.getItem('slug'))
-      .then((res) => {
-        setComments(res.data.comments)
-      })
-      .catch((err) => {
-        console.log(err);
-      })) 
-      : (getComment(localStorage.getItem('slug'))
-      .then((res) => {
-        setComments(res.data.comments)
-      })
-      .catch((err) => {
-        console.log(err);
-      })) 
-  },[localStorage.getItem('slug')])
+	useEffect(() => {
+		getIdArticle(id).then(res => {
+			setArticleData(res.data);
+		});
+	}, [id]);
 
+	return (
+		<div className="article-page">
+			<ArticleTitle data={Articledata} />
+			<ArticleContainer data={Articledata} />
+		</div>
+	);
+};
 
-  return (
-    <div className="article-page">
-      <ArticleTitle data={data}/>
-      <ArticleContainer data={data}/>
-    </div>
-  )
-}
-
-export default ArticleDetail
+export default ArticleDetail;
