152c1348aced4c9ee3498bc809f5803ebc756a1f
hoyyChoi <hoyeon8165@gmail.com>
2023-09-05 17:58:37 +0900

    fix : prettier

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import { BrowserRouter } from "react-router-dom";
-import {RecoilRoot} from 'recoil'
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import {BrowserRouter} from 'react-router-dom';
+import {RecoilRoot} from 'recoil';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  
-    <React.StrictMode>
-      <RecoilRoot>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </RecoilRoot>
-    </React.StrictMode>
- 
-)
+	<React.StrictMode>
+		<RecoilRoot>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</RecoilRoot>
+	</React.StrictMode>,
+);
