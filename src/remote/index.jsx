1916232f003947aa7cca79c062b65e79911e7fd1
hoyyChoi <hoyeon8165@gmail.com>
2023-09-05 19:16:53 +0900

    feat : putArticle

--- src/remote/index.jsx
+++ src/remote/index.jsx
@@ -8,5 +8,12 @@ const getGlobalArticles = () => kusitmsAxios.get('/api/v1/articles');
 const getIdArticle = Id => kusitmsAxios.get(`/api/v1/articles/${Id}`);
 const deleteIdArticle = Id => kusitmsAxios.delete(`/api/v1/articles/${Id}`);
 const postArticle = data => kusitmsAxios.post('/api/v1/articles', data);
+const putArticle = data => kusitmsAxios.put('/api/v1/articles', data);
 
-export {getIdArticle, deleteIdArticle, postArticle, getGlobalArticles};
+export {
+	getIdArticle,
+	deleteIdArticle,
+	postArticle,
+	getGlobalArticles,
+	putArticle,
+};
